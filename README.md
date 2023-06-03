5902582565af8b6df903dcc113948113ce6a0347
Sivayogeith <sivayogeith@gmail.com>
2023-06-03 19:19:42 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This repo is for this blog:
+[How to Pause a Video at a Specific Second](https://ulagellam.com/blog/pausing-video-specific-second-guide)
---
+++ react-ts/src/App.tsx
@@ -0,0 +1,44 @@
+import { useState, createRef } from "react";
+import rickRoll from "/rickroll.mp4";
+import "./App.css";
+
+function App() {
+	const targetTimestamp = 6;
+	const [timestamp, setTimestamp] = useState(0);
+	const [didRickRollCome, setDidRickRollCome] = useState(false);
+
+	const videoRef: any = createRef();
+	const handleTimeUpdate = () => {
+		const currentTime = videoRef["current"]["currentTime"];
+		setTimestamp(currentTime);
+		if (currentTime >= targetTimestamp && !didRickRollCome) {
+			onTimestamp();
+		}
+	};
+
+	const onTimestamp = () => {
+		// When the timestamp is at 6 seconds do this:
+		videoRef["current"].pause();
+		setDidRickRollCome(true);
+	};
+	return (
+		<>
+			<div>
+				<p>The Video is at {timestamp} seconds</p>
+				{didRickRollCome ? (
+					<p>Passed {targetTimestamp} seconds!</p>
+				) : null}
+				<video
+					src={rickRoll}
+					width="750"
+					height="500"
+					ref={videoRef}
+					onTimeUpdate={handleTimeUpdate}
+					controls
+				></video>
+			</div>
+		</>
+	);
+}
+
+export default App;
